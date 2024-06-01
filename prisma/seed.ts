dab431e6c8020a5a57a81a97bef5f93b851d325f
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-06-01 18:45:38 +0200

    update database tables seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,9 @@
 /* eslint-disable prettier/prettier */
-import { PrismaClient, UserRole, UserStatus, Transport, AdStatus, FileType } from '@prisma/client';
+import { PrismaClient, UserRole, UserStatus, Transport, AdStatus, FileType, UserAdStatus } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
-// Generate random role and status based on enums
 const generateRandomRole = (): UserRole => {
     const roles = Object.values(UserRole);
     return faker.helpers.arrayElement(roles) as UserRole;
@@ -13,6 +12,11 @@ const generateRandomRole = (): UserRole => {
 const generateRandomStatus = (): UserStatus => {
     const statuses = Object.values(UserStatus);
     return faker.helpers.arrayElement(statuses) as UserStatus;
+};
+
+const generateUserAdStatus = (): UserAdStatus => {
+    const statuses = Object.values(UserAdStatus);
+    return faker.helpers.arrayElement(statuses) as UserAdStatus;
 };
 
 const generateUsers = (count: number) => {
@@ -58,30 +62,38 @@ const generateCategories = (count: number) => {
     return categories;
 };
 
-const generateAds = (userId: string, categoryIds: string[], count: number) => {
+const generateAds = async (userId: string, categoryIds: string[], count: number) => {
     const ads = [];
     for (let i = 0; i < count; i++) {
-        ads.push({
-            id: faker.string.uuid(),
-            title: faker.lorem.words(5).slice(0, 50),  // Ensure title does not exceed 50 characters
-            description: faker.lorem.paragraph(),
-            startTime: faker.date.future(),
-            endTime: faker.date.future(),
-            duration: faker.number.int({ min: 1, max: 8 }),
-            address: faker.location.streetAddress(),
-            postalCode: faker.location.zipCode(),
-            city: faker.location.city(),
-            country: faker.location.country(),
-            attendees: faker.number.int({ min: 0, max: 100 }),
-            transport: faker.helpers.arrayElement(['car', 'van']) as Transport,
-            conform: faker.datatype.boolean(),
-            status: faker.helpers.arrayElement(['cancel', 'report']) as AdStatus,
-            adPicture: faker.image.url().slice(0, 50),  // Ensure adPicture does not exceed 50 characters
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            userId,
-            categoryId: faker.helpers.arrayElement(categoryIds),  // Randomly assign a categoryId from pre-created categories
-        });
+        const adId = faker.string.uuid();
+        try {
+            const createdAd = await prisma.ad.create({
+                data: {
+                    id: adId,
+                    title: faker.lorem.words(5).slice(0, 50),
+                    description: faker.lorem.paragraph(),
+                    startTime: faker.date.future(),
+                    endTime: faker.date.future(),
+                    duration: faker.number.int({ min: 1, max: 8 }),
+                    address: faker.location.streetAddress(),
+                    postalCode: faker.location.zipCode(),
+                    city: faker.location.city(),
+                    country: faker.location.country().slice(0, 20),
+                    attendees: faker.number.int({ min: 0, max: 100 }),
+                    transport: faker.helpers.arrayElement(['car', 'van']) as Transport,
+                    conform: faker.datatype.boolean(),
+                    status: faker.helpers.arrayElement(['cancel', 'report']) as AdStatus,
+                    adPicture: faker.image.url().slice(0, 50),
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    userId,
+                    categoryId: faker.helpers.arrayElement(categoryIds),
+                },
+            });
+            ads.push(createdAd);
+        } catch (error) {
+            console.error('Error creating ad:', error);
+        }
     }
     return ads;
 };
@@ -101,29 +113,154 @@ const generateFiles = (userId: string, count: number) => {
     return files;
 };
 
+const generateMessages = (userId: string, count: number) => {
+    const messages = [];
+    for (let i = 0; i < count; i++) {
+        messages.push({
+            id: faker.string.uuid(),
+            text: faker.lorem.words(5).slice(0, 50),
+            conform: faker.datatype.boolean(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            userId,
+        });
+    }
+    return messages;
+};
+
+const generateSubjects = (count: number) => {
+    const subjects = [];
+    for (let i = 0; i < count; i++) {
+        subjects.push({
+            id: faker.string.uuid(),
+            name: faker.lorem.words(2),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    }
+    return subjects;
+};
+
+const generateChildren = (count: number) => {
+    const children = [];
+    for (let i = 0; i < count; i++) {
+        children.push({
+            id: faker.string.uuid(),
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            school: faker.commerce.department(),
+            class: faker.helpers.arrayElement(['A', 'B', 'C']),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    }
+    return children;
+};
+
+const generateUserHasSubjects = (userId: string, subjectIds: string[]) => {
+    const userHasSubjects = [];
+    for (const subjectId of subjectIds) {
+        userHasSubjects.push({
+            userId,
+            subjectId,
+        });
+    }
+    return userHasSubjects;
+};
+
+const generateUserHasChildren = (userId, childIds) => {
+    const result = [];
+    childIds.forEach(childId => {
+        const pair = { userId: userId, childId: childId };
+        result.push(pair);
+    });
+    return result;
+};
+
+const generateUserHasAds = (userId: string, adIds: string[]) => {
+    const userHasAds = [];
+    for (const adId of adIds) {
+        userHasAds.push({
+            userId,
+            adId,
+            userAttendees: faker.number.int({ min: 0, max: 6 }),
+            status: generateUserAdStatus(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    }
+    return userHasAds;
+};
+
+const generateAdHasFiles = (adId: string, fileIds: string[]) => {
+    const adHasFiles = [];
+    for (const fileId of fileIds) {
+        adHasFiles.push({
+            adId,
+            fileId,
+        });
+    }
+    return adHasFiles;
+};
+
 const seed = async () => {
-    const categories = generateCategories(5);  // Generate 5 categories
+    const categories = generateCategories(5);
     const createdCategories = await Promise.all(categories.map(category => prisma.category.create({ data: category })));
     const categoryIds = createdCategories.map(category => category.id);
+
+    const subjects = generateSubjects(5);
+    const createdSubjects = await Promise.all(subjects.map(subject => prisma.subject.create({ data: subject })));
+
+    const children = generateChildren(5);
+    const createdChildren = await Promise.all(children.map(child => prisma.child.create({ data: child })));
 
     const users = generateUsers(10);
     for (const user of users) {
         const createdUser = await prisma.user.create({ data: user });
 
-        // Create Profile for each user
         const profile = generateProfiles(createdUser.id);
         await prisma.profile.create({ data: profile });
 
-        // Create Ads for each user
-        const ads = generateAds(createdUser.id, categoryIds, 3);  // Assume 3 ads per user
+        const ads = await generateAds(createdUser.id, categoryIds, 3);
+        const adIds = ads.map(ad => ad.id);
+
         for (const ad of ads) {
-            await prisma.ad.create({ data: ad });
+            const files = generateFiles(createdUser.id, 2);
+            const createdFiles = await Promise.all(files.map(file => prisma.file.create({ data: file })));
+
+            const adHasFiles = generateAdHasFiles(ad.id, createdFiles.map(file => file.id));
+            await Promise.all(adHasFiles.map(adHasFile => prisma.adHasFile.create({ data: adHasFile })));
         }
 
-        // Create Files for each user
-        const files = generateFiles(createdUser.id, 2);  // Assume 2 files per user
-        for (const file of files) {
-            await prisma.file.create({ data: file });
+        const messages = generateMessages(createdUser.id, 3);
+        for (const message of messages) {
+            await prisma.message.create({ data: message });
+        }
+
+        const userChildren = createdChildren.map(child => ({
+            userId: createdUser.id,
+            childId: child.id,
+        }));
+        await prisma.userHasChildren.createMany({ data: userChildren });
+
+        const userHasSubjects = generateUserHasSubjects(createdUser.id, createdSubjects.map(subject => subject.id));
+        await prisma.userHasSubjects.createMany({ data: userHasSubjects });
+
+        const userHasAds = generateUserHasAds(createdUser.id, adIds);
+        await prisma.userHasAds.createMany({ data: userHasAds });
+
+        const userHasChildren = generateUserHasChildren(createdUser.id, createdChildren.map(child => child.id));
+        
+        try {
+            for (const relation of userHasChildren) {
+            await prisma.userHasChildren.upsert({
+                where: { userId_childId: { userId: relation.userId, childId: relation.childId } },
+                update: {},
+                create: relation,
+            });
+            }
+        } catch (error) {
+            console.error('Erreur à la création:', error);
         }
     }
     console.log('Données générées!');
