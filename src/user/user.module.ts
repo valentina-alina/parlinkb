55d8f366f7ec934513f2a2b124ee8861fc8c4222
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-06-13 14:20:32 +0200

    updated develop with Redis cache logic

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AppCacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AppCacheModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
