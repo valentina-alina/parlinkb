b8fccc840940464fbd78a0a144b42356a586ba12
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-06-18 11:07:01 +0200

    updated auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import {
     Controller,
     Get,
     HttpException,
+    HttpStatus,
     Post,
     Req,
     UnauthorizedException,
@@ -15,6 +16,8 @@ import { Request as ExpressRequest } from 'express';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { AuthRefreshGuard } from '../../src/guards/refresh.jwt.guards';
+import { User } from "@prisma/client";
+import { CustomException } from "src/exceptions/custom.exception";
 
 //TODO: EMAIL | ACCOUNT VERIFICATION | USER SIGNIN
 //TODO: USER SIGNOUT
@@ -40,12 +43,13 @@ export class AuthController {
     @Post('register')
     async signup(
         @Body() data: { firstName: string, lastName: string, email: string, password: string}
-    ): Promise<any> {
+    ): Promise<{ user: User, message: string}> {
         const user = await this.userService.findByUnique({
             email: data.email
         })
 
-        if(user) return new HttpException('Server error', 401)
+        // if(user) throw new HttpException('Server error', 401)
+        if(user) throw new CustomException('L\'utilisateur existe déjà', HttpStatus.CONFLICT, "UC-create-1")
 
         data.password = await this.authService.hash(data.password);
 
@@ -53,35 +57,39 @@ export class AuthController {
 
         delete new_user.password;
 
+        const message = `Utilisateur créé`;
+
         return {
             user: new_user,
-            message: "utilisateur créé",
+            message
         }
     }
 
     @Post('login')
     async signin(
         @Body() data: { email: string, password: string}
-    ): Promise<any> {
+    ): Promise<{ access_token: string, refresh_token: string, user: User}> {
         const user = await this.userService.findByUnique({
             email: data.email
         })
         
-        if(!user) throw new HttpException('Les identifiants ne correspondent pas - test1', 401)
+        if(!user) throw new HttpException('Erreur', HttpStatus.CONFLICT)
 
         const isValid = await bcrypt.compare(data.password, user.password)
 
-        if (!isValid) throw new HttpException('Les identifiants ne correspondent pas - test2', 401)
+        if (!isValid) throw new HttpException('Erreur', HttpStatus.CONFLICT)
 
         const payload = { sub: user.id, email: user.email}
 
         delete user.password;
 
+        const access_token = await this.jwtService.signAsync(payload, { secret: process.env.JWT_SECRET, expiresIn: '20m'})
+
         const refresh_token = await this.jwtService.signAsync(payload, { secret: process.env.JWT_REFRESH_TOKEN, expiresIn: '7d'});
 
         this.userService.update({ id: payload.sub }, { refreshToken: refresh_token });
         return {
-            access_token: await this.jwtService.signAsync(payload, { secret: process.env.JWT_SECRET, expiresIn: '20m'}),
+            access_token,
             refresh_token,
             user
         }
@@ -91,10 +99,10 @@ export class AuthController {
     @Post('signout')
     async logout(
         @Req() req: Request
-    ) {
+    ):Promise <{message: string}> {
         const userId = String(req.user.sub);
         const user = await this.userService.findByUnique({id: userId})
-        if(!user) throw new HttpException('server error', 401)
+        if(!user) throw new HttpException('Erreur', HttpStatus.CONFLICT)
 
         this.userService.update({id: user.id}, {refreshToken: ''})
         return {
@@ -106,7 +114,7 @@ export class AuthController {
     @Get('refresh_token')
     async refreshTokens(
         @Req() req: Request
-    ) {
+    ): Promise<{ access_token: string, refresh_token: string, user: User }> {
         const user = await this.userService.findByRefreshToken(req.refreshToken)
         if(!user) throw new UnauthorizedException('server error')
 
