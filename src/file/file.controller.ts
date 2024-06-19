12b12bc930e41bb896fbb80ed8f5dd31c9f9102e
Valentina <valentina.raileanu@outlook.fr>
2024-06-19 23:52:42 +0200

    crud file

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,41 +1,94 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpStatus, HttpException } from '@nestjs/common';
 import { FileService } from './file.service';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
-
-//TODO: USER CREATE FILE
-//TODO: USER READ ALL FILES
-//TODO: USER READ FILES BY PARAMS --> UPDATE | DELETE [FINDBYPARAMS]
-//TODO: USER READ FILES BY ID --> UPDATE | DELETE [FINDBYUNIQUE]
-//TODO: USER DELETE FILES
+import { Prisma, File } from '@prisma/client';
 
 @Controller('file')
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+  private readonly dataName = 'file';
+
+  constructor(private readonly service: FileService) {}
 
   @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.fileService.create(createFileDto);
+  async create(@Body() inputDto: CreateFileDto): Promise<{ [key: string]: File | string }> {
+    const out = await this.service.create(inputDto);
+    const message = 'New file was created';
+    return {
+      [this.dataName]: out,
+      message,
+    };
   }
 
+
   @Get()
-  findAll() {
-    return this.fileService.findAll();
+  async findAllByParams(@Query() query: { skip?: string, take?: string }): Promise<{ [key: string]: File[] | string }> {
+    const prismaOptions: Prisma.FileFindManyArgs = {};
+    if (query.skip) prismaOptions.skip = +query.skip;
+    if (query.take) prismaOptions.take = +query.take;
+
+    const out = await this.service.findAllByParams(prismaOptions);
+    const message = `All files`;
+    return {
+      [this.dataName]: out,
+      message
+    };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fileService.findOne(+id);
+  async readRoute(@Param('id') id: string): Promise<{ [key: string]: File | string }> {
+    const out = await this.service.findByUnique({ id });
+
+    if (!out) throw new HttpException('Le sujet n\'a pas été trouvé', HttpStatus.CONFLICT);
+    
+    const message = `File avec l'id ${id}`;
+    return {
+      [this.dataName]: out,
+      message
+    };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-    return this.fileService.update(+id, updateFileDto);
+  async update(@Param('id') id: string, @Body() inputDto: UpdateFileDto): Promise<{ [key: string]: File | string }> {
+    const out = await this.service.update({ id }, inputDto);
+    const message = `File avec l'id ${id} a été mis à jour`;
+    return {
+      [this.dataName]: out,
+      message
+    };
   }
 
+  // @Delete(':id')
+  // async remove(@Param('id') id: string): Promise<File | string> {
+  //   const file = await this.service.findByUnique({ id });
+
+  //   if (!file) throw new HttpException('Le sujet n\'a pas été trouvé', HttpStatus.CONFLICT);
+
+  //   const out = await this.service.delete({ id });
+
+  //   const message = `File avec l'id ${id} a été supprimé`;
+  //   return {out, 
+  //     message
+  //   };
+  // }
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fileService.remove(+id);
+  async remove(@Param('id') id: string): Promise<{ file?: File; message: string }> {
+    const file = await this.service.findByUnique({ id });
+
+    if (!file) {
+      throw new HttpException('Le sujet n\'a pas été trouvé', HttpStatus.NOT_FOUND);
+    }
+
+    const result = await this.service.delete({ id });
+
+    if (typeof result === 'string') {
+      // Si result est un message d'erreur, lancer une exception HTTP
+      throw new HttpException(result, HttpStatus.CONFLICT);
+    }
+
+    const message = `File avec l'id ${id} a été supprimé`;
+    return { file: result, message };
   }
 }
+
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,26 +3,93 @@ import { Injectable } from '@nestjs/common';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
 
-//TODO:
+import { Prisma, File } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PRISMA_ERRORS } from '../../prisma/prisma.errors';
+
 @Injectable()
 export class FileService {
-  create(createFileDto: CreateFileDto) {
-    return 'This action adds a new file';
+ 
+
+  constructor(
+    private readonly prisma: PrismaService,
+  
+  ) {}
+// inputDto doit avoir la meme structure que  File de Promise sinon ça ne marche pas
+  async create(inputDto: CreateFileDto): Promise<File | string> {
+    try {
+      const out = await this.prisma.file.create({ 
+        data: inputDto
+      });
+      return out;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        const errorMessage = PRISMA_ERRORS[error.code]
+          ? `Prisma error-${error.code}: ${PRISMA_ERRORS[error.code]}`
+          : `Unexpected error: ${error.message}`;
+        return errorMessage;
+      }
+      return `Unexpected error: ${error.message}`;
+    }
   }
 
-  findAll() {
-    return `This action returns all file`;
+  async findAllByParams(options: Prisma.FileFindManyArgs): Promise<File[] | string> {
+    try {
+      return await this.prisma.file.findMany(options);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        const errorMessage = PRISMA_ERRORS[error.code] ? `Prisma error-${error.code}: ${PRISMA_ERRORS[error.code]}` : `Unexpected error: ${error.message}`;
+        return errorMessage;
+      }
+      return `Unexpected error: ${error.message}`;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} file`;
+  async findByUnique(whereUniqueInput: Prisma.FileWhereUniqueInput): Promise<File | string> {
+    try {
+      const out= await this.prisma.file.findUnique({
+        where: whereUniqueInput,
+      });
+      return out
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        const errorMessage = PRISMA_ERRORS[error.code] ? `Prisma error-${error.code}: ${PRISMA_ERRORS[error.code]}` : `Unexpected error: ${error.message}`;
+        return errorMessage;
+      }
+      return `Unexpected error: ${error.message}`;
+    }
   }
 
-  update(id: number, updateFileDto: UpdateFileDto) {
-    return `This action updates a #${id} file`;
+  async update(where: Prisma.FileWhereUniqueInput, data: Prisma.FileUpdateInput): Promise<File | string> {
+    try {
+      const out = await this.prisma.file.update({
+        where,
+        data,
+      });
+      return out;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        const errorMessage = PRISMA_ERRORS[error.code] ? `Prisma error-${error.code}: ${PRISMA_ERRORS[error.code]}` : `Unexpected error: ${error.message}`;
+        return errorMessage;
+      }
+      return `Unexpected error: ${error.message}`;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} file`;
+  async delete(where: Prisma.FileWhereUniqueInput): Promise<File | string> {
+    try {
+      const output = await this.prisma.file.delete({
+        where,
+      });
+      return output;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        const errorMessage = PRISMA_ERRORS[error.code] ? `Prisma error-${error.code}: ${PRISMA_ERRORS[error.code]}` : `Unexpected error: ${error.message}`;
+        return errorMessage;
+      }
+      return `Unexpected error: ${error.message}`;
+    }
   }
 }
+
