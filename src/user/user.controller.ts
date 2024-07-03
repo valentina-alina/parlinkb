badc04c5e16d165416f790bac6f31daf2bd26037
Valentina <valentina.raileanu@outlook.fr>
2024-07-03 10:23:31 +0200

    maj usercontroler et auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -28,43 +28,7 @@ export class UserController {
     private authService: AuthService
   ) {}
 
-//   hashData(data: string) {
-//     return bcrypt.hash(data, 10);
-// }
 
-   // //TODO: USER
-    @Post('login')
-    async signin(
-        @Body() data: { email: string, password: string }
-    ): Promise<{ access_token: string, refresh_token: string, user: User, message: string }> {
-        const user = await this.userService.findByUnique({
-            email: data.email
-        })
-
-        if (!user) throw new HttpException('Erreur: identifiants incorrects, HttpStatus.CONFLICT)
-
-        const isValid = await bcrypt.compare(data.password, user.password)
-
-        if (!isValid) throw new HttpException(`Erreur : Identifiants incorrects`, HttpStatus.CONFLICT)
-
-        const payload = { sub: user.id, email: user.email }
-
-        delete user.password;
-
-        const access_token = await this.jwtService.signAsync(payload, { secret: process.env.JWT_SECRET, expiresIn: '20m' })
-
-        const refresh_token = await this.jwtService.signAsync(payload, { secret: process.env.JWT_REFRESH_TOKEN, expiresIn: '7d' });
-
-        this.userService.update({ id: payload.sub }, { refreshToken: refresh_token });
-
-        const message = `Vous êtes bien connecté`;
-        return {
-            access_token,
-            refresh_token,
-            user,
-            message
-        }
-    }
 
   // @Get()
   // async findAllByParams(@Query() options: {skip?: string, take?: string }): Promise<{users: User[], message: string}> {
@@ -81,6 +45,7 @@ export class UserController {
   //   };
   // }
 
+// trouver user by id
   @Get(':id')
   async readRoute(
       @Param('id') id: string,
@@ -97,6 +62,7 @@ export class UserController {
     };
   }
 
+  // changer password pour user by id
   @Put(':id')
   async updateRoute(
     @Param('id') id: string,
